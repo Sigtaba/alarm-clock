7ee721bc5d923e505c143be2591001f1336635e1
Ethan Luts and Isabella Abatgis <ethan+isabella@>
2017-06-27 16:51:14 -0700

    final push

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,12 +1,12 @@
 var Clock = require('./../js/alarm.js').alarmModule;
 
 $(document).ready(function(){
+  $("#time").text(moment());
   $("#alarmForm").submit(function(event) {
     event.preventDefault();
     var newClock = new Clock();
     var userAlarm = $("#setAlarm").val();
     var countdown = setInterval(alarmTimer, 1000);
-
     function alarmTimer() {
       console.log(newClock.setAlarm(userAlarm));
       if (newClock.setAlarm(userAlarm) === true) {
